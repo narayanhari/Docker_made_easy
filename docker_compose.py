49bc1c38d3df75ada9b1daf9ef962c91f84cdb3b
narayanhari <narayanhari1997@gmail.com>
2020-04-30 10:32:16 -0400

    wordrepess added

--- docker_compose.py
+++ docker_compose.py
@@ -12,9 +12,10 @@ while True:
 		1- Download and install Docker-compose
 		2- Create Workspace 
 		3- Go to Workspace
-		4- Configure Docker-compose file
-		5- Run Docker-compose
-		6- Go to previous Menu""")
+		4- Launch Wordpress in one click
+		5- Configure Docker-compose file
+		6- Run Docker-compose
+		7- Go to previous Menu""")
 	x=int(input("Enter your choice  : "))
 	if x==1:
 		os.system('curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose')
@@ -29,11 +30,15 @@ while True:
 		os.chdir(dir_name)
 		input("Enter to Continue")
 	elif x==4:
+		os.system("docker-compose up -d")
+	elif x==5:
 		os.system("gedit docker-compose.yml")
 		input("Enter to Continue")
-	elif x==5:
-		os.system("docker-compose up -d")
 	elif x==6:
+		dir_name=input("Enter path of Workspace  : ")
+		os.chdir(dir_name)
+		os.system("docker-compose up -d")
+	elif x==7:
 		break
 	else:
 		print("Pls Enter above option only")
